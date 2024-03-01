54d1c83bd4dae09a37300d688c99c7003c5eaeb9
EL MEHDI EL BANNOUNI <44529843+mehdibny@users.noreply.github.com>
2024-03-01 02:25:29 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using e_commece_API_React.Data;
+using e_commece_API_React.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_commece_API_React.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+            
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts() 
+        {
+            return await _context.Products.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>>  GetProduct(int id)
+        {
+           return await _context.Products.FindAsync(id);
+        }
+
+    }
+}
---
+++ Data/StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using e_commece_API_React.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_commece_API_React.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
