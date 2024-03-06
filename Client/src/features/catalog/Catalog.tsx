aa109f81940e654ee58f78c925e32b3fc6836f2c
EL MEHDI EL BANNOUNI <44529843+mehdibny@users.noreply.github.com>
2024-03-06 03:26:10 +0100

    End of section 3

---
+++ Client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,24 @@
+import { Product } from "../../app/models/product"
+import ProductList from "./ProductList";
+import { useState, useEffect } from "react";
+
+
+
+export default function Catalog(){
+    const [products, setProducts]= useState<Product[]>([]);
+
+
+    useEffect(()=>{
+      fetch('https://localhost:7111/api/Products')
+      .then(response =>response.json())
+      .then(data => setProducts(data));
+    },[])
+
+
+    return(
+        <>
+            <ProductList products={products}/>
+        </>
+        
+    )
+}
