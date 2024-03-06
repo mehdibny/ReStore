b8c4ae7e549dc8900bd954ea2580a84fa3b2a481
EL MEHDI EL BANNOUNI <44529843+mehdibny@users.noreply.github.com>
2024-03-06 16:21:48 +0100

    End of section 4

---
+++ Client/src/features/contact/ContactPage.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Contactage(){
+    return(
+        <Typography variant="h2">
+            Contact page
+        </Typography>
+    )
+}
